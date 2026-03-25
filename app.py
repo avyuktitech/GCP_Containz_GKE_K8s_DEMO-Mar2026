9ad2f78f56e2fd5b2f4350f07183a8c760456208
avyuktitech <76204004+avyuktitech@users.noreply.github.com>
2026-03-25 13:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #This app.py is a simple Flask application that serves as a demonstration 
 # for CI/CD to Google Kubernetes Engine (GKE). 
 # It has two routes: the root route ("/") which returns a message indicating that CI/CD to GKE is working, and a health check route ("/health") which returns an "OK" status with a 200 HTTP status code. The application listens on all available interfaces on port 8080.
-
+#Test Comment
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')  # Root route that returns a message indicating CI/CD to GKE is working
 def home():
     return "CI/CD to GKE is Working!"
+    
 
 @app.route('/health')  # Health check route that returns an "OK" status with a 200 HTTP status code
 def health():
